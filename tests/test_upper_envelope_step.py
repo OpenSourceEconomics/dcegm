309af62290f5f57d42fe8dddf0201544ad88fe4a
MaxBlesch <maximilian.blesch@hu-berlin.de>
2022-12-12 16:18:48 +0100

    ADded fixtures instead of code.

--- tests/test_upper_envelope_step.py
+++ tests/test_upper_envelope_step.py
@@ -8,9 +8,9 @@ to the true values of current_policy and current_value.
 import pickle as pkl
 from pathlib import Path
 
+import pytest
 from dcegm.upper_envelope_step import do_upper_envelope_step
 from numpy.testing import assert_array_almost_equal as aaae
-
 
 # Obtain the test directory of the package.
 TEST_DIR = Path(__file__).parent
@@ -18,42 +18,45 @@ TEST_DIR = Path(__file__).parent
 # Directory with additional resources for the testing harness
 TEST_RESOURCES_DIR = TEST_DIR / "resources"
 
-# inputs do_upper_envelope_step
-current_policy_input = pkl.load(
-    open(TEST_RESOURCES_DIR / "current_policy_input.pkl", "rb")
-)
-current_value_input = pkl.load(
-    open(TEST_RESOURCES_DIR / "current_value_input.pkl", "rb")
-)
-choice = 0
-n_grid_wealth = 500
-compute_value_input = pkl.load(
-    open(TEST_RESOURCES_DIR / "compute_value_input.pkl", "rb")
-)
 
-# apply function do_upper_envelope_step to given inputs
-current_policy, current_value = do_upper_envelope_step(
-    current_policy_input,
-    current_value_input,
-    choice,
-    n_grid_wealth,
-    compute_value_input,
-)
+@pytest.fixture()
+def data():
+    out = {}
+    # inputs do_upper_envelope_step
+    current_policy_input = pkl.load(
+        open(TEST_RESOURCES_DIR / "current_policy_input.pkl", "rb")
+    )
+    current_value_input = pkl.load(
+        open(TEST_RESOURCES_DIR / "current_value_input.pkl", "rb")
+    )
+    choice = 0
+    n_grid_wealth = 500
+    compute_value_input = pkl.load(
+        open(TEST_RESOURCES_DIR / "compute_value_input.pkl", "rb")
+    )
 
-# correct outputs
-current_policy_output = pkl.load(
-    open(TEST_RESOURCES_DIR / "current_policy_output.pkl", "rb")
-)
-current_value_output = pkl.load(
-    open(TEST_RESOURCES_DIR / "current_value_output.pkl", "rb")
-)
+    # apply function do_upper_envelope_step to given inputs
+    out["current_policy"], out["current_value"] = do_upper_envelope_step(
+        current_policy_input,
+        current_value_input,
+        choice,
+        n_grid_wealth,
+        compute_value_input,
+    )
+    return out
 
 
 # compare current_policy to true value
-def test_upper_envelope_step_policy():
-    aaae(current_policy, current_policy_output)
+def test_upper_envelope_step_policy(data):
+    aaae(
+        data["current_policy"],  # correct outputs
+        pkl.load(open(TEST_RESOURCES_DIR / "current_policy_output.pkl", "rb")),
+    )
 
 
 # compare current_value to true value
-def test_upper_envelope_step_value():
-    aaae(current_value, current_value_output)
+def test_upper_envelope_step_value(data):
+    aaae(
+        data["current_value"],
+        pkl.load(open(TEST_RESOURCES_DIR / "current_value_output.pkl", "rb")),
+    )
