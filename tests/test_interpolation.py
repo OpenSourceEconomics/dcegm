6dfa34daf92cc94aaf94100429f5dd2fee6cc7dc
Sebastian Gsell <sebastian.gsell93@gmail.com>
2022-12-16 13:18:42 +0100

    Refactor test module.

--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -7,36 +7,40 @@ function interp1d.
 
 """
 import numpy as np
+import pytest
 from dcegm.interpolate import linear_interpolation_with_extrapolation
 from dcegm.interpolate import linear_interpolation_with_inserting_missing_values
 from numpy.testing import assert_allclose
 from scipy.interpolate import interp1d
 
 
-"""draw random test arrays from a uniform distribution"""
-# fix length of input arrays
-n = 5
-m = 7
-# random test arrays
-x = np.random.rand(n)
-y = np.random.rand(n)
-x_new = np.random.rand(m)
-missing_value = np.random.rand(1)
+@pytest.fixture()
+def random_test_data():
+    """Draw random test arrays from a uniform distribution."""
+    n = 5
+    m = 7
+
+    x = np.random.rand(n)
+    y = np.random.rand(n)
+    x_new = np.random.rand(m)
+    missing_value = np.random.rand(1)
+
+    return x, y, x_new, missing_value
 
 
-# test linear_interpolation_with_extrapolation
-def test_linear_interpolation_with_extrapolation():
-    assert_allclose(
-        linear_interpolation_with_extrapolation(x, y, x_new),
-        interp1d(x, y, fill_value="extrapolate")(x_new),
-        atol=1e-10,
-    )
+def test_linear_interpolation_with_extrapolation(random_test_data):
+    x, y, x_new, _ = random_test_data
+
+    got = linear_interpolation_with_extrapolation(x, y, x_new)
+    expected = interp1d(x, y, fill_value="extrapolate")(x_new)
+
+    assert_allclose(got, expected, atol=1e-10)
 
 
-# test linear_interpolation_with_missing_values
-def test_linear_interpolation_with_missing_values():
-    assert_allclose(
-        linear_interpolation_with_inserting_missing_values(x, y, x_new, missing_value),
-        interp1d(x, y, bounds_error=False, fill_value=missing_value)(x_new),
-        atol=1e-10,
-    )
+def test_linear_interpolation_with_missing_values(random_test_data):
+    x, y, x_new, missing_value = random_test_data
+
+    got = linear_interpolation_with_inserting_missing_values(x, y, x_new, missing_value)
+    expected = interp1d(x, y, bounds_error=False, fill_value=missing_value)(x_new)
+
+    assert_allclose(got, expected, atol=1e-10)
