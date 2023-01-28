e9345c12d87dfd89880e28c0547a4c94e3f45c14
MaxBlesch <maximilian.blesch@hu-berlin.de>
2023-01-28 20:48:44 +0100

    Polished the budget function.

--- src/toy_models/consumption_retirement_model/budget_functions.py
+++ src/toy_models/consumption_retirement_model/budget_functions.py
@@ -2,11 +2,10 @@ from typing import Dict
 
 import jax
 import jax.numpy as jnp
-import numpy as np
 
 
 def budget_constraint(
-    state: np.ndarray,
+    state: jnp.ndarray,
     saving: float,
     income_shock: float,
     params_dict: dict,
@@ -22,37 +21,36 @@ def budget_constraint(
         params_dict (dict): Dictionary containing model parameters.
         options (dict): Options dictionary.
         income_shock (float): Stochastic shock on labor income; may or may not be
-         normally distributed. Entry of income_shock_draws.
+            normally distributed. Entry of income_shock_draws.
 
     Returns:
-        (np.ndarray): 2d array of shape (n_quad_stochastic, n_grid_wealth)
-            containing all possible next period wealths.
+        beginning_period_wealth (float): The current beginning of period resources.
 
     """
     r = params_dict["interest_rate"]
 
     # Calculate stochastic labor income
-    _next_period_income = _calc_stochastic_income(
+    income_from_last_period = _calc_stochastic_income(
         state,
         wage_shock=income_shock,
         params_dict=params_dict,
         options=options,
     )
 
-    _next_period_wealth = _next_period_income + (1 + r) * saving
+    beginning_period_wealth = income_from_last_period + (1 + r) * saving
 
     # Retirement safety net, only in retirement model, but we require to have it always
     # as a parameter
-    _next_period_wealth = jnp.maximum(
-        _next_period_wealth, params_dict["consumption_floor"]
+    beginning_period_wealth = jnp.maximum(
+        beginning_period_wealth, params_dict["consumption_floor"]
     )
 
-    return _next_period_wealth
+    return beginning_period_wealth
 
 
 @jax.jit
 def _calc_stochastic_income(
-    child_state: np.ndarray,
+    state: jnp.ndarray,
     wage_shock: float,
     params_dict: dict,
     options: Dict[str, int],
@@ -70,7 +68,7 @@ def _calc_stochastic_income(
     respectively. Note that the last one (alpha_2) typically has a negative sign.
 
     Args:
-        child_state (np.ndarray): 1d array of shape (n_state_variables,) denoting
+        state (jnp.ndarray): 1d array of shape (n_state_variables,) denoting
             the current child state.
         wage_shock (float): Stochastic shock on labor income; may or may not be normally
             distributed. Entry of income_shock_draws.
@@ -79,20 +77,20 @@ def _calc_stochastic_income(
         options (dict): Options dictionary.
 
     Returns:
-        stochastic_income (np.ndarray): 1d array of shape (n_quad_points,) containing
-            potential end of period incomes. It consists of a deterministic component,
-            i.e. age-dependent labor income, and a stochastic shock.
+        stochastic_income (float): The potential end of period income. It consists of a
+            deterministic component, i.e. age-dependent labor income,
+            and a stochastic shock.
 
     """
     # For simplicity, assume current_age - min_age = experience
     min_age = options["min_age"]
-    age = child_state[0] + min_age
+    age = state[0] + min_age
 
     # Determinisctic component of income depending on experience:
     # constant + alpha_1 * age + alpha_2 * age**2
     exp_coeffs = jnp.array(
         [params_dict["constant"], params_dict["exp"], params_dict["exp_squared"]]
     )
-    labor_income = exp_coeffs @ (age ** np.arange(len(exp_coeffs)))
+    labor_income = exp_coeffs @ (age ** jnp.arange(len(exp_coeffs)))
     working_income = jnp.exp(labor_income + wage_shock)
-    return (1 - child_state[1]) * working_income
+    return (1 - state[1]) * working_income
