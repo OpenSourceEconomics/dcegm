aa58fbcb40f5eed23962002f907e3973f79f74f4
MaxBlesch <maximilian.blesch@hu-berlin.de>
2023-07-11 18:48:31 +0200

    Further refactor.

--- src/dcegm/fast_upper_envelope.py
+++ src/dcegm/fast_upper_envelope.py
@@ -298,172 +298,111 @@ def scan_value_function(
             policy_j = policy[i + 1]
             exog_grid_j = endog_grid_j - policy_j
 
-        else:
-            # if right turn is made and jump registered
-            # remove point or perform forward scan
-            if grad_before > grad_next and grad_next > grad_next_forward:
-                intersect_grid, intersect_value = _linear_intersection(
-                    x1=endog_grid[idx_next_on_lower_curve],
-                    y1=value[idx_next_on_lower_curve],
-                    x2=endog_grid_j,
-                    y2=value_j,
-                    x3=endog_grid[i + 1],
-                    y3=value[i + 1],
-                    x4=endog_grid[idx_before_on_upper_curve],
-                    y4=value[idx_before_on_upper_curve],
-                )
+        elif grad_before > grad_next or grad_next < grad_next_backward:
+            intersect_grid, intersect_value = _linear_intersection(
+                x1=endog_grid[idx_next_on_lower_curve],
+                y1=value[idx_next_on_lower_curve],
+                x2=endog_grid_j,
+                y2=value_j,
+                x3=endog_grid[i + 1],
+                y3=value[i + 1],
+                x4=endog_grid[idx_before_on_upper_curve],
+                y4=value[idx_before_on_upper_curve],
+            )
 
-                intersect_policy_left = _evaluate_point_on_line(
-                    x1=endog_grid[idx_next_on_lower_curve],
-                    y1=policy[idx_next_on_lower_curve],
-                    x2=endog_grid_j,
-                    y2=policy_j,
-                    point_to_evaluate=intersect_grid,
-                )
-                intersect_policy_right = _evaluate_point_on_line(
-                    x1=endog_grid[i + 1],
-                    y1=policy[i + 1],
-                    x2=endog_grid[idx_before_on_upper_curve],
-                    y2=policy[idx_before_on_upper_curve],
-                    point_to_evaluate=intersect_grid,
-                )
+            intersect_policy_left = _evaluate_point_on_line(
+                x1=endog_grid[idx_next_on_lower_curve],
+                y1=policy[idx_next_on_lower_curve],
+                x2=endog_grid_j,
+                y2=policy_j,
+                point_to_evaluate=intersect_grid,
+            )
+            intersect_policy_right = _evaluate_point_on_line(
+                x1=endog_grid[i + 1],
+                y1=policy[i + 1],
+                x2=endog_grid[idx_before_on_upper_curve],
+                y2=policy[idx_before_on_upper_curve],
+                point_to_evaluate=intersect_grid,
+            )
 
-                value_refined[idx_refined] = intersect_value
-                policy_refined[idx_refined] = intersect_policy_left
-                endog_grid_refined[idx_refined] = intersect_grid
-                idx_refined += 1
+            value_refined[idx_refined] = intersect_value
+            policy_refined[idx_refined] = intersect_policy_left
+            endog_grid_refined[idx_refined] = intersect_grid
+            idx_refined += 1
+
+            value_refined[idx_refined] = intersect_value
+            policy_refined[idx_refined] = intersect_policy_right
+            endog_grid_refined[idx_refined] = intersect_grid
+            idx_refined += 1
+
+            value_refined[idx_refined] = value[i + 1]
+            policy_refined[idx_refined] = policy[i + 1]
+            endog_grid_refined[idx_refined] = endog_grid[i + 1]
+            idx_refined += 1
+
+            value_k = value_j
+            endog_grid_k = endog_grid_j
+            exog_grid_k = exog_grid_j
+            policy_k = policy_j
+
+            value_j = value[i + 1]
+            endog_grid_j = endog_grid[i + 1]
+            policy_j = policy[i + 1]
+            exog_grid_j = endog_grid_j - policy_j
+
+            # k = j
+            # j = i + 1
+
+        elif grad_next > grad_next_backward:
+            intersect_grid, intersect_value = _linear_intersection(
+                x1=endog_grid_j,
+                y1=value_j,
+                x2=endog_grid_k,
+                y2=value_k,
+                x3=endog_grid[i + 1],
+                y3=value[i + 1],
+                x4=endog_grid[idx_before_on_upper_curve],
+                y4=value[idx_before_on_upper_curve],
+            )
+
+            # The next two interpolations is just to show that from
+            # interpolation from each side leads to the same result
+            intersect_policy_left = _evaluate_point_on_line(
+                x1=endog_grid_k,
+                y1=policy_k,
+                x2=endog_grid_j,
+                y2=policy_j,
+                point_to_evaluate=intersect_grid,
+            )
+            intersect_policy_right = _evaluate_point_on_line(
+                x1=endog_grid[i + 1],
+                y1=policy[i + 1],
+                x2=endog_grid[idx_before_on_upper_curve],
+                y2=policy[idx_before_on_upper_curve],
+                point_to_evaluate=intersect_grid,
+            )
+
+            if idx_before_on_upper_curve > 0 and i > 1:
+                value_refined[idx_refined - 1] = intersect_value
+                policy_refined[idx_refined - 1] = intersect_policy_left
+                endog_grid_refined[idx_refined - 1] = intersect_grid
 
                 value_refined[idx_refined] = intersect_value
                 policy_refined[idx_refined] = intersect_policy_right
                 endog_grid_refined[idx_refined] = intersect_grid
                 idx_refined += 1
 
-                value_refined[idx_refined] = value[i + 1]
-                policy_refined[idx_refined] = policy[i + 1]
-                endog_grid_refined[idx_refined] = endog_grid[i + 1]
-                idx_refined += 1
+            value_refined[idx_refined] = value[i + 1]
+            policy_refined[idx_refined] = policy[i + 1]
+            endog_grid_refined[idx_refined] = endog_grid[i + 1]
+            idx_refined += 1
 
-                value_k = value_j
-                endog_grid_k = endog_grid_j
-                exog_grid_k = exog_grid_j
-                policy_k = policy_j
+            value_j = intersect_value
+            endog_grid_j = intersect_grid
+            policy_j = intersect_policy_right
+            exog_grid_j = endog_grid_j - policy_j
 
-                value_j = value[i + 1]
-                endog_grid_j = endog_grid[i + 1]
-                policy_j = policy[i + 1]
-                exog_grid_j = endog_grid_j - policy_j
-
-                # k = j
-                # j = i + 1
-
-            # if left turn is made or right turn with no jump, then
-            # keep point provisionally and conduct backward scan
-            else:
-                if grad_next > grad_next_backward:
-                    intersect_grid, intersect_value = _linear_intersection(
-                        x1=endog_grid_j,
-                        y1=value_j,
-                        x2=endog_grid_k,
-                        y2=value_k,
-                        x3=endog_grid[i + 1],
-                        y3=value[i + 1],
-                        x4=endog_grid[idx_before_on_upper_curve],
-                        y4=value[idx_before_on_upper_curve],
-                    )
-
-                    # The next two interpolations is just to show that from
-                    # interpolation from each side leads to the same result
-                    intersect_policy_left = _evaluate_point_on_line(
-                        x1=endog_grid_k,
-                        y1=policy_k,
-                        x2=endog_grid_j,
-                        y2=policy_j,
-                        point_to_evaluate=intersect_grid,
-                    )
-                    intersect_policy_right = _evaluate_point_on_line(
-                        x1=endog_grid[i + 1],
-                        y1=policy[i + 1],
-                        x2=endog_grid[idx_before_on_upper_curve],
-                        y2=policy[idx_before_on_upper_curve],
-                        point_to_evaluate=intersect_grid,
-                    )
-
-                    if idx_before_on_upper_curve > 0 and i > 1:
-                        value_refined[idx_refined - 1] = intersect_value
-                        policy_refined[idx_refined - 1] = intersect_policy_left
-                        endog_grid_refined[idx_refined - 1] = intersect_grid
-
-                        value_refined[idx_refined] = intersect_value
-                        policy_refined[idx_refined] = intersect_policy_right
-                        endog_grid_refined[idx_refined] = intersect_grid
-                        idx_refined += 1
-
-                    value_refined[idx_refined] = value[i + 1]
-                    policy_refined[idx_refined] = policy[i + 1]
-                    endog_grid_refined[idx_refined] = endog_grid[i + 1]
-                    idx_refined += 1
-
-                    value_j = intersect_value
-                    endog_grid_j = intersect_grid
-                    policy_j = intersect_policy_right
-                    exog_grid_j = endog_grid_j - policy_j
-
-                    # j = i + 1
-
-                elif grad_next < grad_next_backward:
-                    intersect_grid, intersect_value = _linear_intersection(
-                        x1=endog_grid[idx_next_on_lower_curve],
-                        y1=value[idx_next_on_lower_curve],
-                        x2=endog_grid_j,
-                        y2=value_j,
-                        x3=endog_grid[i + 1],
-                        y3=value[i + 1],
-                        x4=endog_grid[idx_before_on_upper_curve],
-                        y4=value[idx_before_on_upper_curve],
-                    )
-
-                    intersect_policy_left = _evaluate_point_on_line(
-                        x1=endog_grid[idx_next_on_lower_curve],
-                        y1=policy[idx_next_on_lower_curve],
-                        x2=endog_grid_j,
-                        y2=policy_j,
-                        point_to_evaluate=intersect_grid,
-                    )
-                    intersect_policy_right = _evaluate_point_on_line(
-                        x1=endog_grid[i + 1],
-                        y1=policy[i + 1],
-                        x2=endog_grid[idx_before_on_upper_curve],
-                        y2=policy[idx_before_on_upper_curve],
-                        point_to_evaluate=intersect_grid,
-                    )
-
-                    value_refined[idx_refined] = intersect_value
-                    policy_refined[idx_refined] = intersect_policy_left
-                    endog_grid_refined[idx_refined] = intersect_grid
-                    idx_refined += 1
-
-                    value_refined[idx_refined] = intersect_value
-                    policy_refined[idx_refined] = intersect_policy_right
-                    endog_grid_refined[idx_refined] = intersect_grid
-                    idx_refined += 1
-
-                    value_refined[idx_refined] = value[i + 1]
-                    policy_refined[idx_refined] = policy[i + 1]
-                    endog_grid_refined[idx_refined] = endog_grid[i + 1]
-                    idx_refined += 1
-
-                    value_k = value_j
-                    endog_grid_k = endog_grid_j
-                    exog_grid_k = exog_grid_j
-                    policy_k = policy_j
-
-                    value_j = value[i + 1]
-                    endog_grid_j = endog_grid[i + 1]
-                    policy_j = policy[i + 1]
-                    exog_grid_j = endog_grid_j - policy_j
-
-                    # k = j
-                    # j = i + 1
+            # j = i + 1
 
     value_refined[idx_refined] = value[-1]
     endog_grid_refined[idx_refined] = endog_grid[-1]
