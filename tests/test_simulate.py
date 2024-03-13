fbbbb7e9c8347e6d101f51b6b4241532f34b8a1f
Sebastian Gsell <sebastian.gsell93@gmail.com>
2024-03-13 09:23:51 +0100

    Add test

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -7,9 +7,31 @@ import numpy as np
 import pytest
 from dcegm.simulation.sim_utils import create_simulation_df
 from dcegm.simulation.simulate import simulate_all_periods
+from dcegm.simulation.simulate import simulate_all_periods_for_model
 from dcegm.simulation.simulate import simulate_final_period
 from dcegm.simulation.simulate import simulate_single_period
 from numpy.testing import assert_array_almost_equal as aaae
+
+
+def _create_test_objects_from_df(df, params):
+    _cond = [df["choice"] == 0, df["choice"] == 1]
+    _val = [df["taste_shock_0"], df["taste_shock_1"]]
+    df["taste_shock_selected_choice"] = np.select(_cond, _val)
+
+    value_period_zero = (
+        df.xs(0, level=0)["utility"].mean()
+        + params["beta"]
+        * (
+            df.xs(1, level=0)["utility"]
+            + df.xs(1, level=0)["taste_shock_selected_choice"]
+        ).mean()
+    )
+    expected = (
+        df.xs(0, level=0)["value"].mean()
+        - df.xs(0, level=0)["taste_shock_selected_choice"].mean()
+    )
+
+    return value_period_zero, expected
 
 
 @pytest.fixture()
@@ -63,6 +85,8 @@ def model_setup(toy_model_exog_ltc):
         "model_funcs": model_funcs,
         "map_state_choice_to_index": map_state_choice_to_index,
         "get_next_period_state": get_next_period_state,
+        "initial_states": initial_states,
+        "initial_resources": initial_resources,
         "initial_states_and_resources": initial_states_and_resources,
         "sim_specific_keys": sim_specific_keys,
         "seed": seed,
@@ -73,6 +97,7 @@ def test_simulate_lax_scan(model_setup):
     params = model_setup["params"]
     options = model_setup["options"]
     choice_range = jnp.arange(options["model_params"]["n_choices"])
+
     state_space_names = model_setup["state_space_names"]
     value = model_setup["value"]
     policy_left = model_setup["policy_left"]
@@ -83,7 +108,10 @@ def test_simulate_lax_scan(model_setup):
     model_funcs = model_setup["model_funcs"]
     map_state_choice_to_index = model_setup["map_state_choice_to_index"]
 
-    initial_states_and_resources = model_setup["initial_states_and_resources"]
+    initial_states_and_resources = (
+        model_setup["initial_states"],
+        model_setup["initial_resources"],
+    )
     sim_specific_keys = model_setup["sim_specific_keys"]
 
     simulate_body = partial(
@@ -155,7 +183,7 @@ def test_simulate_lax_scan(model_setup):
 def test_simulate(model_setup):
     params = model_setup["params"]
     options = model_setup["options"]
-    jnp.arange(options["model_params"]["n_choices"])
+
     state_space_names = model_setup["state_space_names"]
     value = model_setup["value"]
     policy_left = model_setup["policy_left"]
@@ -166,11 +194,8 @@ def test_simulate(model_setup):
     model_funcs = model_setup["model_funcs"]
     map_state_choice_to_index = model_setup["map_state_choice_to_index"]
 
-    initial_states_and_resources = model_setup["initial_states_and_resources"]
-    initial_states, initial_resources = (
-        initial_states_and_resources[0],
-        initial_states_and_resources[1],
-    )
+    initial_states = model_setup["initial_states"]
+    initial_resources = model_setup["initial_resources"]
 
     seed = model_setup["seed"]
 
@@ -199,29 +224,56 @@ def test_simulate(model_setup):
 
     df = create_simulation_df(result)
 
-    _cond = [df["choice"] == 0, df["choice"] == 1]
-    _val = [df["taste_shock_0"], df["taste_shock_1"]]
-    df["taste_shock_selected_choice"] = np.select(_cond, _val)
-
-    value_period_zero = (
-        df.xs(0, level=0)["utility"].mean()
-        + params["beta"]
-        * (
-            df.xs(1, level=0)["utility"]
-            + df.xs(1, level=0)["taste_shock_selected_choice"]
-        ).mean()
-    )
-    expected = (
-        df.xs(0, level=0)["value"].mean()
-        - df.xs(0, level=0)["taste_shock_selected_choice"].mean()
-    )
-
+    value_period_zero, expected = _create_test_objects_from_df(df, params)
     ids_violating_absorbing_retirement = df.query(
         "(period == 0 and choice == 1) and (period == 1 and choice == 0)"
     )
-    assert len(ids_violating_absorbing_retirement) == 0
 
+    assert len(ids_violating_absorbing_retirement) == 0
     aaae(value_period_zero.mean(), expected.mean(), decimal=2)
 
 
-# def test_simulate_all_periods_for_model():
+def test_simulate_all_periods_for_model(model_setup):
+    states_initial = model_setup["initial_states"]
+    resources_initial = model_setup["initial_resources"]
+    n_periods = model_setup["options"]["state_space"]["n_periods"]
+    params = model_setup["params"]
+    seed = model_setup["seed"]
+    endog_grid_solved = model_setup["endog_grid"]
+    value_solved = model_setup["value"]
+    policy_left_solved = model_setup["policy_left"]
+    policy_right_solved = model_setup["policy_right"]
+    choice_range = jnp.arange(
+        model_setup["map_state_choice_to_index"].shape[-1], dtype=jnp.int16
+    )
+    model = {
+        "state_space_names": model_setup["state_space_names"],
+        "map_state_choice_to_index": model_setup["map_state_choice_to_index"],
+        "model_funcs": model_setup["model_funcs"],
+        "exog_mapping": model_setup["exog_state_mapping"],
+        "get_next_period_state": model_setup["get_next_period_state"],
+    }
+
+    result = simulate_all_periods_for_model(
+        states_initial=states_initial,
+        resources_initial=resources_initial,
+        n_periods=n_periods,
+        params=params,
+        seed=seed,
+        endog_grid_solved=endog_grid_solved,
+        value_solved=value_solved,
+        policy_left_solved=policy_left_solved,
+        policy_right_solved=policy_right_solved,
+        choice_range=choice_range,
+        model=model,
+    )
+
+    df = create_simulation_df(result)
+
+    value_period_zero, expected = _create_test_objects_from_df(df, params)
+    ids_violating_absorbing_retirement = df.query(
+        "(period == 0 and choice == 1) and (period == 1 and choice == 0)"
+    )
+
+    assert len(ids_violating_absorbing_retirement) == 0
+    aaae(value_period_zero.mean(), expected.mean(), decimal=2)
