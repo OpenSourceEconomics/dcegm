3d432580fb449717e8543ff261044c8c7cf2f513
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-05-01 16:02:31 +0200

    Restructured interpolation. Unified functions.

--- src/dcegm/egm/interpolate_marginal_utility.py
+++ src/dcegm/egm/interpolate_marginal_utility.py
@@ -2,14 +2,12 @@ from typing import Callable
 from typing import Dict
 from typing import Tuple
 
-from dcegm.interpolation import get_index_high_and_low
-from dcegm.interpolation import interp_value_and_check_creditconstraint
-from dcegm.interpolation import linear_interpolation_formula
+from dcegm.interpolation import interp_value_and_policy_on_wealth
 from jax import numpy as jnp
 from jax import vmap
 
 
-def interpolate_value_and_calc_marginal_utility(
+def interpolate_value_and_marg_utility_on_next_period_wealth(
     compute_marginal_utility: Callable,
     compute_utility: Callable,
     state_choice_vec: Dict[str, int],
@@ -51,104 +49,32 @@ def interpolate_value_and_calc_marginal_utility(
             containing the interpolated value function.
 
     """
-    # For all choices, the wealth is the same in the solution
-    ind_high, ind_low = get_index_high_and_low(
-        x=endog_grid_child_state_choice, x_new=wealth_beginning_of_period
-    )
-    marg_utils, value_interp = vmap(
+
+    def interp_on_single_wealth(wealth):
+        policy_interp, value_interp = interp_value_and_policy_on_wealth(
+            wealth=wealth,
+            policy=policy_child_state_choice,
+            value=value_child_state_choice,
+            endog_grid=endog_grid_child_state_choice,
+            compute_utility=compute_utility,
+            endog_grid_min=endog_grid_child_state_choice[1],
+            value_at_zero_wealth=value_child_state_choice[0],
+            state_choice_vec=state_choice_vec,
+            params=params,
+        )
+        marg_utility_interp = compute_marginal_utility(
+            consumption=policy_interp, params=params, **state_choice_vec
+        )
+        return marg_utility_interp, value_interp
+
+    vector_interp_func = vmap(
         vmap(
-            _interpolate_value_and_marg_util,
-            in_axes=(0, 0, 0, 0, 0, 0, 0, None, None, None, None, None, None),
+            interp_on_single_wealth,
+            in_axes=(0,),
         ),
-        in_axes=(0, 0, 0, 0, 0, 0, 0, None, None, None, None, None, None),
-    )(
-        jnp.take(policy_child_state_choice, ind_high),
-        value_child_state_choice[ind_high],
-        endog_grid_child_state_choice[ind_high],
-        jnp.take(policy_child_state_choice, ind_low),
-        value_child_state_choice[ind_low],
-        endog_grid_child_state_choice[ind_low],
-        wealth_beginning_of_period,
-        compute_utility,
-        compute_marginal_utility,
-        endog_grid_child_state_choice[1],
-        value_child_state_choice[0],
-        state_choice_vec,
-        params,
+        in_axes=(0,),
     )
+
+    marg_utils, value_interp = vector_interp_func(wealth_beginning_of_period)
 
     return marg_utils, value_interp
-
-
-def _interpolate_value_and_marg_util(
-    policy_high: float | jnp.ndarray,
-    value_high: float | jnp.ndarray,
-    wealth_high: float | jnp.ndarray,
-    policy_low: float | jnp.ndarray,
-    value_low: float | jnp.ndarray,
-    wealth_low: float | jnp.ndarray,
-    new_wealth: float | jnp.ndarray,
-    compute_utility: Callable,
-    compute_marginal_utility: Callable,
-    endog_grid_min: float | jnp.ndarray,
-    value_at_zero_wealth: float | jnp.ndarray,
-    state_choice_vec: Dict[str, int],
-    params: Dict[str, float],
-) -> Tuple[float, float]:
-    """Calculate interpolated marginal utility and value function.
-
-    Args:
-        policy_high (float): Policy function value at the higher end of the
-            interpolation interval.
-        value_high (float): Value function value at the higher end of the
-            interpolation interval.
-        wealth_high (float): Endogenous wealth grid value at the higher end of the
-            interpolation interval.
-        policy_low (float): Policy function value at the lower end of the
-            interpolation interval.
-        value_low (float): Value function value at the lower end of the
-            interpolation interval.
-        wealth_low (float): Endogenous wealth grid value at the lower end of the
-            interpolation interval.
-        new_wealth (float): New endogenous wealth grid value.
-        compute_marginal_utility (callable): Function for calculating the marginal
-            utility from consumption level. The input ```params``` is already
-            partialled in.
-        endog_grid_min (float): Minimum endogenous wealth grid value.
-        value_min (float): Minimum value function value.
-        choice (int): Discrete choice of an agent.
-        params (dict): Dictionary containing the model parameters.
-
-    Returns:
-        tuple:
-
-        - marg_util_interp (float): Interpolated marginal utility function.
-        - value_interp (float): Interpolated value function.
-
-    """
-    policy_interp = linear_interpolation_formula(
-        y_high=policy_high,
-        y_low=policy_low,
-        x_high=wealth_high,
-        x_low=wealth_low,
-        x_new=new_wealth,
-    )
-
-    value_interp = interp_value_and_check_creditconstraint(
-        value_high=value_high,
-        wealth_high=wealth_high,
-        value_low=value_low,
-        wealth_low=wealth_low,
-        new_wealth=new_wealth,
-        compute_utility=compute_utility,
-        endog_grid_min=endog_grid_min,
-        value_at_zero_wealth=value_at_zero_wealth,
-        state_choice_vec=state_choice_vec,
-        params=params,
-    )
-
-    marg_utility_interp = compute_marginal_utility(
-        consumption=policy_interp, params=params, **state_choice_vec
-    )
-
-    return marg_utility_interp, value_interp
--- src/dcegm/interpolation.py
+++ src/dcegm/interpolation.py
@@ -1,5 +1,6 @@
 from typing import Callable
 from typing import Dict
+from typing import Tuple
 
 import jax.numpy as jnp
 from jax import numpy as jnp
@@ -104,3 +105,73 @@ def interp_value_and_check_creditconstraint(
     )
 
     return value_interp
+
+
+def interp_value_and_policy_on_wealth(
+    wealth: float | jnp.ndarray,
+    policy: float | jnp.ndarray,
+    value: float | jnp.ndarray,
+    endog_grid: float | jnp.ndarray,
+    compute_utility: Callable,
+    endog_grid_min: float | jnp.ndarray,
+    value_at_zero_wealth: float | jnp.ndarray,
+    state_choice_vec: Dict[str, int],
+    params: Dict[str, float],
+) -> Tuple[float, float]:
+    """Calculate interpolated marginal utility and value function.
+
+    Args:
+        policy_high (float): Policy function value at the higher end of the
+            interpolation interval.
+        value_high (float): Value function value at the higher end of the
+            interpolation interval.
+        wealth_high (float): Endogenous wealth grid value at the higher end of the
+            interpolation interval.
+        policy_low (float): Policy function value at the lower end of the
+            interpolation interval.
+        value_low (float): Value function value at the lower end of the
+            interpolation interval.
+        wealth_low (float): Endogenous wealth grid value at the lower end of the
+            interpolation interval.
+        wealth (float): New endogenous wealth grid value.
+        compute_marginal_utility (callable): Function for calculating the marginal
+            utility from consumption level. The input ```params``` is already
+            partialled in.
+        endog_grid_min (float): Minimum endogenous wealth grid value.
+        value_min (float): Minimum value function value.
+        choice (int): Discrete choice of an agent.
+        params (dict): Dictionary containing the model parameters.
+
+    Returns:
+        tuple:
+
+        - marg_util_interp (float): Interpolated marginal utility function.
+        - value_interp (float): Interpolated value function.
+
+    """
+
+    # For all choices, the wealth is the same in the solution
+    ind_high, ind_low = get_index_high_and_low(x=endog_grid, x_new=wealth)
+
+    policy_interp = linear_interpolation_formula(
+        y_high=policy[ind_high],
+        y_low=policy[ind_low],
+        x_high=endog_grid[ind_high],
+        x_low=endog_grid[ind_low],
+        x_new=wealth,
+    )
+
+    value_interp = interp_value_and_check_creditconstraint(
+        value_high=value[ind_high],
+        wealth_high=endog_grid[ind_high],
+        value_low=value[ind_low],
+        wealth_low=endog_grid[ind_low],
+        new_wealth=wealth,
+        compute_utility=compute_utility,
+        endog_grid_min=endog_grid_min,
+        value_at_zero_wealth=value_at_zero_wealth,
+        state_choice_vec=state_choice_vec,
+        params=params,
+    )
+
+    return policy_interp, value_interp
