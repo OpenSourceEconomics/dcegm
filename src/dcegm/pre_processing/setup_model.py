d1c69927732b932c31581fa80495283010bc691a
MaxBlesch <maximilian.blesch@hu-berlin.de>
2024-06-01 18:12:45 +0200

    Deleted hardcoded assigned dtypes.

--- src/dcegm/pre_processing/setup_model.py
+++ src/dcegm/pre_processing/setup_model.py
@@ -61,7 +61,7 @@ def setup_model(
     )
 
     model_funcs["exog_state_mapping"] = create_exog_state_mapping(
-        model_structure["exog_state_space"].astype(np.int64),
+        model_structure["exog_state_space"],
         model_structure["exog_states_names"],
     )
 
@@ -136,9 +136,8 @@ def load_and_setup_model(
         budget_constraint=budget_constraint,
     )
 
-    exog_state_space = model["model_structure"]["exog_state_space"]
     model["model_funcs"]["exog_state_mapping"] = create_exog_state_mapping(
-        exog_state_space=np.array(exog_state_space, dtype=np.int64),
+        exog_state_space=model["model_structure"]["exog_state_space"],
         exog_names=model["model_structure"]["exog_states_names"],
     )
 
