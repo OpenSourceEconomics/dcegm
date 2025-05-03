84899b20216f41f00ef041f7ad5d3727e6ba04fd
MaxBlesch <maximilianblesch@web.de>
2025-05-03 21:59:37 +0200

    Done with restructuring.

--- tests/test_models/two_period_models/model.py
+++
@@ -1,9 +0,0 @@
-from jax import numpy as jnp
-
-
-def prob_exog_health(health, params):
-    prob_good_health = (health == 0) * 0.7 + (health == 1) * 0.3 + (health == 2) * 0.2
-    prob_medium_health = (health == 0) * 0.2 + (health == 1) * 0.5 + (health == 2) * 0.2
-    prob_bad_health = (health == 0) * 0.1 + (health == 1) * 0.2 + (health == 2) * 0.6
-
-    return jnp.array([prob_good_health, prob_medium_health, prob_bad_health])
