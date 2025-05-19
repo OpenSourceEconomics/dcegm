f4fcc3338c6ff605c5ccb2497ecc4c962683fdfc
Annica Gehlen <ama.gehlen@gmail.com>
2025-05-19 18:33:38 +0200

    remove failed extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,6 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.extlinks",  # link to websites using abbreviations
     "sphinx.ext.napoleon",
-    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
